1799021fc2b398757de401af2b73959a8e9e7724
Leon <phoenixachievement@gmail.com>
2025-12-02 15:44:13 +0100

    Day02.1

--- src/Day02.1.kt
+++ src/Day02.1.kt
@@ -1,7 +1,6 @@
 import java.util.Scanner
 
 var value: Long = 0
-val ids = mutableListOf<Long>()
 
 fun main() {
     val sc = Scanner(System.`in`)
@@ -19,12 +18,12 @@ fun main() {
     }
 
     for (i in 0..firsts.size-1){
+        println(i)
+        if (firsts[i].toString().length%2 == 1 && seconds[i].toString().length%2 == 1){
+            continue
+        }
         for (j in firsts[i]..seconds[i]){
-            if (ids.contains(j)){
-                continue
-            }
             checkValid(j)
-            ids.add(j)
         }
     }
 
@@ -32,15 +31,13 @@ fun main() {
 }
 
 fun checkValid(id: Long) {
-    val numbers: List<Int> = id.toString().map { it.digitToInt() }
-    var valid = false
-    for (i in 0..numbers.size/2-1){
-        if (numbers[i] != numbers[i + numbers.size/2] || numbers.size%2 == 1){
-            valid = true
-            break
-        }
-    }
-    if (!valid){
+    val numbers = id.toString()
+    if (numbers.length%2 == 1) return
+
+    val firstHalf = numbers.substring(0, numbers.length/2)
+    val secondHalf = numbers.substring(numbers.length/2)
+
+    if (firstHalf == secondHalf) {
         value += id
     }
 }
