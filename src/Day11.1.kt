7910634d74d52a6b3c5396792ad9f8e8eaee1ad2
Leon <leon@puhl.tech>
2025-12-11 22:19:00 +0100

    Day11.1 complete

---
+++ src/Day11.1.kt
@@ -0,0 +1,57 @@
+import java.util.Scanner
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    val lines = mutableListOf<Pair<String, MutableList<String>>>()
+    var line = sc.nextLine()
+    while(line.isNotEmpty()){
+        val splittedLine = line.split(" ").toMutableList()
+        val key = splittedLine.removeAt(0).removeSuffix(":")
+        lines.add(Pair(key, splittedLine))
+        line = sc.nextLine()
+    }
+
+    val rootNode = lines.removeAt(lines.indexOfFirst { it.first == "you" })
+    val root = Node(rootNode.first)
+    rootNode.second.forEach { root.addChild(makeTree(lines, it)) }
+
+    println(findLeafs("out", root))
+
+}
+
+fun makeTree(lines: MutableList<Pair<String, MutableList<String>>>, key: String): Node<String> {
+    if (key.equals("out") || key.equals("you")) return Node(key)
+    val thisNode = lines[lines.indexOfFirst { it.first == key }]
+    val node = Node(thisNode.first)
+    thisNode.second.forEach { if (!node.alreadyAParent(Node(it))) node.addChild(makeTree(lines, it)) }
+    return node
+}
+
+fun findLeafs(key: String, node: Node<String>): Int {
+    if (node.value!!.equals(key)) return 1
+    var count = 0
+    node.children.forEach { count += (findLeafs(key, it)) }
+    return count
+}
+
+data class Node<String>(var value: String) {
+    var parent: Node<String>? = null
+    var children: MutableList<Node<String>> = mutableListOf()
+
+    fun addChild(node: Node<String>) {
+        children.add(node)
+        node.parent = this
+    }
+    fun hasChildren(): Boolean = children.isNotEmpty()
+
+    fun alreadyAParent(node: Node<String>): Boolean {
+        var tempNode = Node(node.value)
+        while (tempNode.parent != null) {
+            if (tempNode.parent!!.value == node.value) return true
+            tempNode = tempNode.parent!!
+        }
+        return false
+    }
+
+
+}
