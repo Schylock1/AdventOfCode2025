3e2d67537fd849b3a4150a946616f13fc5371405
Leon <leon@puhl.tech>
2025-12-17 17:03:51 +0100

    Day06.2 complete

--- src/Day06.2.kt
+++ src/Day06.2.kt
@@ -10,22 +10,54 @@ fun main(){
     }
     var operatorLine = lists.removeLast()
 
-    println(operatorLine)
-    println(lists)
-
     var result: Long = 0
+    var operator = operatorLine.first()
+    val listOfNumbersToCountWith = mutableListOf<Int>()
+    var firstTime = true
 
-    while (operatorLine.isNotEmpty()){
-        var operator = operatorLine.first()
+    fun endSingleProblem(){
+        var toAddToFinal: Long = 0
+        if (operator.equals('+')){
+            listOfNumbersToCountWith.forEach { toAddToFinal += it }
+            println("+ with: $toAddToFinal")
+        } else if(operator.equals('*')){
+            if (toAddToFinal.toInt() == 0) toAddToFinal++
+            listOfNumbersToCountWith.forEach { toAddToFinal *= it }
+            println("* with: $toAddToFinal")
+        } else throw Exception()
+        result += toAddToFinal
+        listOfNumbersToCountWith.clear()
+
+        if (operatorLine.isNotEmpty()){
+            operator = operatorLine.first()
+        }
+    }
+
+    do {
+        if (!firstTime && (operatorLine.isEmpty() || !operatorLine.first().equals(' '))){
+            endSingleProblem()
+        }
+        firstTime = false
+        if (operatorLine.isEmpty()){
+            break
+        }
         operatorLine = operatorLine.drop(1)
         val listOfSingleLines = mutableListOf<String>()
         repeat(4){ listOfSingleLines.add("") }
-        do {
-            lists.forEachIndexed { i, it ->
-                listOfSingleLines[i] += it.first()
-                lists[i] = lists[i].drop(1)
-            }
+        lists.forEachIndexed { i, it ->
+            listOfSingleLines[i] += it.first()
+            lists[i] = lists[i].drop(1)
+        }
+        var number = ""
+        listOfSingleLines.forEach {
+            if (!it.equals(" ")) number += it
+        }
+        if (number.isNotEmpty()){
+            listOfNumbersToCountWith.add(number.toInt())
+        }
+    } while (true)
 
-        } while (!operatorLine.first().equals(" "))
-    }
+    println(result)
+
+
 }
